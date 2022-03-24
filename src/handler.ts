1f6b23a9434eb45fca7fabc7cdea15dbf98d4793
PC732_nakata_yuki <>
2022-03-25 00:34:07 +0900

    first commit

---
+++ src/handler.ts
@@ -0,0 +1,24 @@
+import { Context, Handler } from 'aws-lambda';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Server } from 'http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, proxy } from 'aws-serverless-express';
+import * as express from 'express';
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);  
+  const nestApp = await NestFactory.create(AppModule, adapter);
+  await nestApp.init();
+  return createServer(expressApp);
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
+  }
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
