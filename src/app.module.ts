0028818d539fcb4a33c002a3ab503954d42e5442
PC732_nakata_yuki <>
2022-03-28 22:27:12 +0900

    wip: right after nest g resouce books

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { BooksModule } from './books/books.module';
+import { BooksService } from './books/books.service';
+import { BooksController } from './books/books.controller';
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [TypeOrmModule.forRoot(), UsersModule, BooksModule],
+  controllers: [UsersController, BooksController],
+  providers: [UsersService, BooksService],
 })
 export class AppModule {}
